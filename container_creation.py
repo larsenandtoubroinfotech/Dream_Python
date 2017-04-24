e061078a45e2542db162c5c60ccb1d43aa2499c4
shefu <guptashefali84@yahoo.com>
2017-04-24 15:05:48 +0530

    redeployemnet script...changes in container_creation.py and created search_parameter_column_command.py

--- container_creation.py
+++ container_creation.py
@@ -2,6 +2,7 @@
 import subprocess
 import sys
 import shutil
+import search_parameter_column_command
 service_name = sys.argv[1].lower()
 image_name = "citi.docker.com:5000/"+service_name
 base_image_name = sys.argv[2]
@@ -10,4 +11,8 @@ shutil.copy("/home/shefali/docker/java_tomcat/dockerfile", ".")
 subprocess.call(["python", "/home/shefali/scripts/python_scripts/replace_words.py", base_image_name, "@image_name", "dockerfile"])
 subprocess.call(["docker", "build", "-f", "dockerfile", "-t", image_name, "."])
 subprocess.call(["docker", "push", image_name])
-subprocess.call(["docker", "service", "create", "--network", "my_network", "-p", "4100:8080", "--name", service_name, image_name])
+status = search_parameter_column_command.search_command_output(service_name)
+if status == "present":
+    subprocess.call(["docker", "service", "update", "--image", image_name, service_name])
+else:
+    subprocess.call(["docker", "service", "create", "--network", "my_network", "-p", "4100:8080", "--name", service_name, image_name])
--- search_parameter_column_command.py
+++ search_parameter_column_command.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 import subprocess
-f = open('command_output', 'w')
-f = subprocess.Popen(["docker", "service", "ls"], stdout=subprocess.PIPE)
+import sys
+#app_name = sys.argv[1]
 
-print f
-#for line in open('command_output', 'w'):
-#commmand_output = subprocess.Popen(["docker", "service", "ls"], stdout=subprocess.PIPE).stdout
-#print commmand_output
+app_list = []
+def search_command_output(app_name):
+    with open('command_output', 'w') as f:
+        f.write(subprocess.check_output(["docker", "service", "ls"]))
+    with open('command_output', 'r') as f:
+        column_no = f.readline().split().index('NAME')
+        for line in f: 
+            app_names = line.split()[column_no]
+            app_list.append(app_names)
+    if app_name in app_list:
+        return 'present'
